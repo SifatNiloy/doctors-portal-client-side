a961259fb52d673b6192abe82a8a502d4628bb42
SifatNiloy <sifatniloy18@gmail.com>
2023-01-04 00:52:24 -0800

    care section added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import InfoCards from '../../InfoCards/InfoCards';
 import Banner from '../Banner/Banner';
+import Care from '../Care/Care';
 import Services from '../Services/Services';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner/>
             <InfoCards></InfoCards>
             <Services></Services>
+            <Care></Care>
         </div>
     );
 };
