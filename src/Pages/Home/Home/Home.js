08440812bee97494b8dc1958c11552fd4d5777c8
SifatNiloy <sifatniloy18@gmail.com>
2023-01-04 18:48:03 -0800

    make appointment component created

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import InfoCards from '../../InfoCards/InfoCards';
 import Banner from '../Banner/Banner';
 import Care from '../Care/Care';
+import MakeAppointment from '../MakeAppointment/MakeAppointment';
 import Services from '../Services/Services';
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
             <InfoCards></InfoCards>
             <Services></Services>
             <Care></Care>
+            <MakeAppointment></MakeAppointment>
         </div>
     );
 };
