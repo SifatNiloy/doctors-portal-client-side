41eccdd2c1ced8b053346d4f20093332bc43cead
SifatNiloy <sifatniloy18@gmail.com>
2023-01-07 23:53:18 -0800

    data loaded with react query

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,16 +1,24 @@
+import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
 import BookingModal from '../BookingModal/BookingModal';
 import AppointmentOption from './AppointmentOption';
 
 const AvailableAppointments = ({ selectedDate }) => {
-    const [appointmentOptions, setAppointmentOptions]=useState([])
-    const [treatment, setTreatment]= useState([])
-    useEffect(()=>{
-        fetch('appointmentOptions.json')
-        .then(res=> res.json())
-        .then(data=> setAppointmentOptions(data))
-    },[])
+    // const [appointmentOptions, setAppointmentOptions]=useState([])
+    const [treatment, setTreatment]= useState([]);
+
+    const { data: appointmentOptions=[]}=useQuery({
+        queryKey: ['appointmentOptions'],
+        queryFn: () => fetch('http://localhost:5000/appointmentOptions')
+            .then(res => res.json())
+    })
+
+    // useEffect(()=>{
+    //     fetch('http://localhost:5000/appointmentOptions')
+    //     .then(res=> res.json())
+    //     .then(data=> setAppointmentOptions(data))
+    // },[])
     return (
         <section className='my-16 px-6'>
             <p className='text-center text-secondary font-bold'>Available Services on {format(selectedDate,'PP')}</p>
