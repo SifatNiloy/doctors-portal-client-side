6d55b38d43be1468b529677cc5b8ff31954627b2
SifatNiloy <sifatniloy18@gmail.com>
2023-01-08 19:31:02 -0800

    responsive dashboard drawer added

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter  } from "react-router-dom"
 import Appointment from "../../Pages/Appointment/Appointment/Appointment"
 import Dashboard from "../../Pages/Dashboard/Dashboard/Dashboard"
 import Home from "../../Pages/Home/Home/Home"
+import DashboardLayout from "../../Pages/Layout/DashboardLayout"
 import Main from "../../Pages/Layout/Main"
 import Login from "../../Pages/Login/Login"
 import SignUp from "../../Pages/SignUp/SignUp"
@@ -26,13 +27,19 @@ export const router= createBrowserRouter([
            },
            {
                path: '/appointment',
-               element: <Appointment></Appointment>   
+               element: <Appointment> </Appointment>   
            },           
                       
         ]        
     },
     {
         path:'/dashboard',
-        element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>
+        element: <PrivateRoute><DashboardLayout> </DashboardLayout> </PrivateRoute>,
+        children: [
+            {
+                path: '/dashboard',
+                element: <Dashboard></Dashboard>
+            }
+        ]
     }
 ])
