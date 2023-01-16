509d50710d24e2ca9c21a94e9681d5fb901b4ef2
SifatNiloy <sifatniloy18@gmail.com>
2023-01-15 22:22:57 -0800

    delele modal added in manage doctor

--- src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors/ManageDoctors.js
@@ -1,35 +1,43 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
+import ConfirmationModal from '../../Shared/ConfirmationModal/ConfirmationModal';
 import Spinner from '../../Shared/Spinner/Spinner';
 
 const ManageDoctors = () => {
-    const {data:doctors , isLoading}= useQuery({
-        queryKey :['doctors'],
-        queryFn: async ()=>{
-            try{
-                const res = await fetch('http://localhost:5000/doctors',{
+    const [deletingDoctor, setDeletingDoctor] = useState(null)
+    const closeModal = () => {
+        setDeletingDoctor(null)
+    }
+    const handleDeleteDoctor = doctor => {
+        console.log(doctor)
+    }
+    const { data: doctors, isLoading } = useQuery({
+        queryKey: ['doctors'],
+        queryFn: async () => {
+            try {
+                const res = await fetch('http://localhost:5000/doctors', {
                     headers: {
                         authorization: `bearer ${localStorage.getItem('accessToken')}`
                     }
                 });
-                const data= res.json();
+                const data = res.json();
                 return data;
 
             }
-            catch(error){
+            catch (error) {
 
             }
         }
     })
-    if(isLoading){
+    if (isLoading) {
         return <Spinner></Spinner>
     }
     return (
         <div>
-           <h2 className="text-3xl">Manage Doctors:{doctors?.length} </h2> 
+            <h2 className="text-3xl">Manage Doctors:{doctors?.length} </h2>
             <div className="overflow-x-auto">
                 <table className="table w-full">
-               
+
                     <thead>
                         <tr>
                             <th></th>
@@ -41,10 +49,10 @@ const ManageDoctors = () => {
                         </tr>
                     </thead>
                     <tbody>
-                   
+
                         {
-                            doctors?.map((doctor , index)=> <tr key={doctor._id}>
-                                <th>{index+1}</th>
+                            doctors?.map((doctor, index) => <tr key={doctor._id}>
+                                <th>{index + 1}</th>
                                 <td>
                                     <div className="avatar">
                                         <div className="w-16 rounded-full">
@@ -55,14 +63,27 @@ const ManageDoctors = () => {
                                 <td>{doctor.name}</td>
                                 <td>{doctor.email}</td>
                                 <td>{doctor.specialty}</td>
-                                <td><button className='btn btn-sm btn-error'>Delete</button></td>
+                                <td>
+                                    <label onClick={() => setDeletingDoctor(doctor)} htmlFor="confirmation-modal" className="btn btn-sm btn-error">Delete</label>
+                                </td>
                             </tr>)
                         }
-                    
-                       
+
+
                     </tbody>
                 </table>
             </div>
+            {
+                deletingDoctor && <ConfirmationModal
+                    title={`Are you sure you want to delete? `}
+                    message={`If you delete ${deletingDoctor.name} it cannot be undone`}
+                    successAction={handleDeleteDoctor}
+                    modalData= {deletingDoctor}
+                    closeModal={closeModal}
+                >
+
+                </ConfirmationModal>
+            }
         </div>
     );
 };
