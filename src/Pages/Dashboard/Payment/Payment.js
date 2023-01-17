f2d468770fc0aaaa4aecec34410f2315e7a14381
SifatNiloy <sifatniloy18@gmail.com>
2023-01-17 11:33:13 -0800

    stripe js added

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -3,10 +3,12 @@ import { useLoaderData } from 'react-router-dom';
 
 const Payment = () => {
     const booking= useLoaderData();
+    const {appointmentDate, slot, patient, treatment, price}=booking;
     console.log('booking data', booking)
     return (
         <div>
-           <h2 className="text-3xl">Payment for {booking.treatment} </h2> 
+           <h2 className="text-3xl">Payment for {treatment} </h2> 
+            <p className='text-xl'>Please pay <strong> ${price}</strong> for your treatment on {appointmentDate} at {slot} </p>
         </div>
     );
 };
