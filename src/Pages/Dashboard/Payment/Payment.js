95b0a6d1f6c59d83528b630ad2807daf84c0141b
SifatNiloy <sifatniloy18@gmail.com>
2023-01-17 21:54:00 -0800

    cardError showed on the ui

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -1,6 +1,8 @@
+import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import CheckoutForm from './CheckoutForm';
 
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
@@ -13,6 +15,11 @@ const Payment = () => {
         <div>
            <h2 className="text-3xl">Payment for {treatment} </h2> 
             <p className='text-xl'>Please pay <strong> ${price}</strong> for your treatment on {appointmentDate} at {slot} </p>
+            <div className='my-12 w-96'>
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm />
+                </Elements>
+            </div>
         </div>
     );
 };
